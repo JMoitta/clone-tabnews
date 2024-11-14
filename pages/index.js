8a5e115a13589d5f235bdd9898665092770e749a
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2024-11-14 21:56:09 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    const mensagem = "Eu amo vc e a nossa Joaninha. Se você me ama, dá uma risadinha!"
+    return <section>
+        <h1>{mensagem}</h1>
+        <p>Com amor Joao (Sr. Manrique)</p>
+    </section>
+}
