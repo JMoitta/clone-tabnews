fb37df258c70399cd6ff84aea7c36424365214a2
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2024-11-19 13:42:46 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 export default function Home() {
-    const mensagem = "Eu amo vc e a nossa Joaninha. Se você me ama, dá uma risadinha!"
-    return <section>
-        <h1>{mensagem}</h1>
-        <p>Com amor Joao (Sr. Manrique)</p>
+  const mensagem =
+    "Eu amo vc e a nossa Joaninha. Se você me ama, dá uma risadinha!";
+  return (
+    <section>
+      <h1>{mensagem}</h1>
+      <p>Com amor Joao (Sr. Manrique)</p>
     </section>
+  );
 }
