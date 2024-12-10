31e685dea44847900662f19f7fc1f8779b4ddf0b
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2024-12-10 13:51:41 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ mensagem: "Alunos do curso.dev são pessoas acima da média" });
+  const updateAt = new Date().toISOString();
+
+  const resultVersion = await database.query("SHOW server_version;");
+  const { server_version: version } = resultVersion.rows[0];
+
+  const resultMaxConnections = await database.query("SHOW max_connections;");
+  const max_connections = Number(resultMaxConnections.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const result = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const opened_connections = result.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version,
+        max_connections,
+        opened_connections,
+      },
+    },
+  });
 }
 
 export default status;
