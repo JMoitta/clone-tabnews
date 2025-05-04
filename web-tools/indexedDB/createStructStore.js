f59564706eb5a2fbba1ef9f947e059888b267662
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2025-05-04 17:33:45 +0000

    corecao de bug ao acessar a index de notas-musicais

--- web-tools/indexedDB/createStructStore.js
+++ web-tools/indexedDB/createStructStore.js
@@ -2,39 +2,43 @@ const dbName = "NotasMusicais";
 const dbVersion = 1;
 
 /** @type {IDBDatabase} */
-export let db;
-export async function createIndexedDB() {
-  const request = window.indexedDB.open(dbName, dbVersion);
+export let getConnection;
+export async function connectionIndexedDB() {
+  if (!getConnection)
+    getConnection = new Promise((resolve) => {
+      const request = window.indexedDB.open(dbName, dbVersion);
 
-  request.onerror = (event) => {
-    console.error("Why didn't you allow my web app to use IndexedDB?!");
-  };
+      request.onerror = (event) => {
+        console.error("Why didn't you allow my web app to use IndexedDB?!");
+      };
 
-  request.onsuccess = (event) => {
-    db = event.target.result;
+      request.onsuccess = (event) => {
+        const db = event.target.result;
+        resolve(db);
 
-    db.onerror = (event) => {
-      // Generic error handler for all errors targeted at this database's
-      // requests!
-      console.error(`Database error: ${event.target.error?.message}`);
-    };
-  };
+        db.onerror = (event) => {
+          // Generic error handler for all errors targeted at this database's
+          // requests!
+          console.error(`Database error: ${event.target.error?.message}`);
+        };
+      };
 
-  request.onupgradeneeded = (event) => {
-    /** @type {IDBDatabase} */
-    const db = event.target.result;
+      request.onupgradeneeded = (event) => {
+        /** @type {IDBDatabase} */
+        const db = event.target.result;
 
-    // Create an objectStore to hold information about our customers. We're
-    // going to use "ssn" as our key path because it's guaranteed to be
-    // unique - or at least that's what I was told during the kickoff meeting.
-    const exercisesObjectStore = db.createObjectStore("exercicios", {
-      keyPath: "id",
-      autoIncrement: true,
+        // Create an objectStore to hold information about our customers. We're
+        // going to use "ssn" as our key path because it's guaranteed to be
+        // unique - or at least that's what I was told during the kickoff meeting.
+        const exercisesObjectStore = db.createObjectStore("exercicios", {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+
+        // exercisesObjectStore.createIndex("date", "date", { unique: false });
+        // exercisesObjectStore.createIndex("time", "time", { unique: false });
+      };
     });
 
-    // exercisesObjectStore.createIndex("date", "date", { unique: false });
-    // exercisesObjectStore.createIndex("time", "time", { unique: false });
-  };
+  return getConnection;
 }
-
-if (typeof window !== "undefined") createIndexedDB();
