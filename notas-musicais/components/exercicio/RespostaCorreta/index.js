6c51607184551d34fcabe010eecac6cfb247e23a
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2025-06-29 00:51:24 +0000

    Atualiza feedback de acerto e erro

--- notas-musicais/components/exercicio/RespostaCorreta/index.js
+++ notas-musicais/components/exercicio/RespostaCorreta/index.js
@@ -2,8 +2,11 @@ import styles from "./index.module.css";
 
 export function RespostaCorreta() {
   return (
-    <div className={`${styles.barraDeStatus} ${styles.barroDeStatusSucesso}`}>
-      Muito bom 👏, continue assim 🚀
+    <div className={styles.container}>
+      <span>😎</span>
+      <div className={`${styles.barraDeStatus} ${styles.barroDeStatusSucesso}`}>
+        Muito bom 👏, continue assim 🚀
+      </div>
     </div>
   );
 }
--- notas-musicais/components/exercicio/RespostaErrada/index.js
+++ notas-musicais/components/exercicio/RespostaErrada/index.js
@@ -2,8 +2,11 @@ import styles from "./index.module.css";
 
 export function RespostaErrada() {
   return (
-    <div className={`${styles.barraDeStatus} ${styles.barroDeStatusFalhou}`}>
-      Poxa 😢, nao fique triste faz parte do processo 🤓
+    <div className={styles.container}>
+      <span>😱</span>
+      <div className={`${styles.barraDeStatus} ${styles.barroDeStatusFalhou}`}>
+        Poxa 😢, nao fique triste faz parte do processo 🤓
+      </div>
     </div>
   );
 }
